d49c114e601ce95dfe8de78a3b9cf68d0cf0e80f
Luxj <358231333@qq.com>
2018-03-09 15:41:29 +0800

    初始化项目

--- app/src/main/java/com/android/yuexunit/yxeducation/MainActivity.kt
+++ app/src/main/java/com/android/yuexunit/yxeducation/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.android.yuexunit.yxeducation
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.android.yuexunit.yxeducationlibrary.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    override fun initData(savedInstanceState: Bundle?) {
+        setTitleText("智慧教育")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        test_name.text = "欢迎来到测试"
     }
+
 }
